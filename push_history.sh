4b021d7bcbcd5ba0a62a8267f52e3a07986e9b32
Cloud User <ec2-user@ip-172-31-21-33.eu-north-1.compute.internal>
2024-09-26 12:52:46 +0000

    2024-09-26 12:52:46

---
+++ push_history.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+export HISTTIMEFORMAT="%F %T"
+source ~/.bashrc
+
+cd /home/ec2-user/cronejob_assignment/training-biz-piyush
+
+cat ~/.bash_history > /home/ec2-user/cronejob_assignment/training-biz-piyush/history.txt
+
+/usr/bin/git add .
+/usr/bin/git commit -m "$(date '+%Y-%m-%d %H:%M:%S')"
+
+/usr/bin/git push origin main
